2964f20f78f2c04932adcdc0d3bb76d28d34c7b0
GUILHERME ROJAS RIBEIRO <u17182@academico.cotuca.unicamp.br>
2018-08-28 09:57:07 -0300

    tens certeza?

---
+++ Pratica-III/Pratica-III/cadastro.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/menu.Master" AutoEventWireup="true" CodeBehind="cadastro.aspx.cs" Inherits="Pratica_III.cadastro" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+        <div class="section">
+            <div class="container white ">
+                <center><h4>Login</h4></center>
+                <div class="row section">
+                    <div class="input-field col s6">
+                        <i class="material-icons prefix tooltipped" data-position="bottom" data-tooltip="Email para Pacientes">account_box</i>
+                        <asp:TextBox placeholder="Cadastro" runat="server" id="txtEmail" type="" class="validate"></asp:TextBox>
+                    </div>           
+                    <div class="input-field col s6">
+                        <i class="material-icons prefix tooltipped" data-position="bottom" data-tooltip="Senha do Cadastro">lock</i>
+                        <asp:TextBox placeholder="Senha" runat="server" id="txtSenha" type="" class="validate"></asp:TextBox>
+                    </div>
+                </div>
+                <br />  
+                <center><asp:Button ID="btnCadastrar" runat="server" Text="Entrar" class="waves-effect waves-light btn-large green darken-1" OnClick="btnCadastrar_Click" /></center>
+            </div>
+        </div>
+</asp:Content>
---
+++ Pratica-III/Pratica-III/cadastro.aspx.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Web.Configuration;
+using Pratica_III.App_Start;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Pratica_III
+{
+    public partial class cadastro : System.Web.UI.Page
+    {   
+        
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnCadastrar_Click(object sender, EventArgs e)
+        {          
+
+            if(String.IsNullOrEmpty(txtEmail.Text) || String.IsNullOrEmpty(txtSenha.Text))
+            {
+                ScriptManager.RegisterClientScriptBlock(this, this.GetType(), "scr", "javascript:M.toast({html: 'Preencha os dados corretamente'});", true);
+                return;
+            }
+
+            String conString = WebConfigurationManager.ConnectionStrings["conexaoBD"].ConnectionString;
+            conexaoBD acessoBD = new conexaoBD();
+            acessoBD.Connection(conString);
+            acessoBD.AbrirConexao();
+
+            String senha = Hash(txtSenha.Text);
+            String sql;
+            int res;
+
+            //começa verificando se é secretária
+            sql = String.Format("SELECT SENHA FROM ADM WHERE NOME = '{0}' AND SENHA = '{1}'",txtEmail.Text, senha);
+            res = acessoBD.ExecutarConsulta(sql);
+            if (res > 0) //tem cadastro
+            {
+                Session["cargo"] = 0;
+                Response.Redirect("index.aspx");
+                return;
+            }
+
+            //verificar se é médico
+            sql = String.Format("SELECT SENHA FROM MEDICO WHERE EMAIL = '{0}' AND SENHA = '{1}'", txtEmail.Text, senha);
+            res = acessoBD.ExecutarConsulta(sql);
+            if (res > 0) //tem cadastro
+            {
+                Session["cargo"] = 1;
+                Response.Redirect("index.aspx");
+                return;
+            }
+
+            //verificar se é paciente
+            sql = String.Format("SELECT SENHA FROM PACIENTE WHERE EMAIL = '{0}' AND SENHA = '{1}'", txtEmail.Text, senha);
+            res = acessoBD.ExecutarConsulta(sql);
+            if (res > 0) //tem cadastro
+            {
+                Session["cargo"] = 2;
+                Response.Redirect("index.aspx");
+                return;
+            }
+
+            ScriptManager.RegisterClientScriptBlock(this, this.GetType(), "scr", "javascript:M.toast({html: 'Você não possui cadastro'});", true);
+        }
+
+        static string Hash(string input)
+        {
+            var hash = (new SHA1Managed()).ComputeHash(Encoding.UTF8.GetBytes(input));
+            return string.Join("", hash.Select(b => b.ToString("x2")).ToArray());
+        }
+    }
+}
