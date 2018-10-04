477278a1b7b441dfac65a44835399ba0bfc1924e
Guilherme Rojas <guiribrojas@gmail.com>
2018-10-04 09:20:00 -0300

    consultas feitas
    falta apenas implementar com o gráfico

--- Pratica-III/Funcoes_Relatorios.sql
+++ Pratica-III/Funcoes_Relatorios.sql
@@ -1,21 +1,29 @@
-CREATE FUNCTION consultaMensalMed (@mes int, @ano int)
-RETURNS TABLE
-AS
-RETURN (
-	SELECT M.nome as nome, count(c.id_medico) as Consultas FROM CONSULTA AS C
-	JOIN MEDICO AS M ON C.id_medico = M.id
-	WHERE
-	YEAR(C.horario) = @ano AND
-	MONTH(C.horario) = @mes
-	GROUP BY M.nome
-);
+--relat1
+SELECT count(*) as quantidade, RIGHT(CONVERT(CHAR(10),horario,103),7) as data 
+	FROM CONSULTA WHERE id_medico = 2 group by RIGHT(CONVERT(CHAR(10),horario,103),7)
+--                      ^ ^ ^ ^ ^ ^ ^
 
-SELECT id,nome FROM MEDICO
+--relat2
+select count(*) as quantidade, e.nome as especialidade
+	from CONSULTA c, MEDICO m, ESPECIALIDADE_MEDICO e
+	where c.id_medico = m.id and m.id_especialidade_medico = e.id and
+			day(c.horario) = 4 and month(c.horario) = 10 and year(c.horario) = 2018
+	group by e.nome --       ^                        ^                        ^
 
---1
-SELECT M.nome, count(c.id_medico) FROM CONSULTA AS C JOIN MEDICO AS M ON C.id_medico = M.id WHERE YEAR(C.horario) = 2018 AND MONTH(C.horario) = 9 GROUP BY M.nome
+--relat3
+select count(c.id_paciente) as quantidade, m.nome as medico
+	from CONSULTA c, MEDICO m
+	where c.id_medico = m.id
+	group by m.nome
 
-select  count(*), DATENAME(month,horario) from CONSULTA group by year(horario),month(horario)
-insert consulta values(GETDATE(),2,8,1,NULL,1)
+--relat4
+select count(*) as quantidade from CONSULTA
+	where concluida = -1 and id_medico = 2
+	group by id_medico --    ^ ^ ^ ^ ^ ^ ^
 
-SELECT count(*) as qtd, RIGHT(CONVERT(CHAR(10),horario,103),7) as data FROM CONSULTA WHERE id_medico = 2 group by year(horario),month(horario)
+
+select * from CONSULTA
+select * from MEDICO
+select * from ESPECIALIDADE_MEDICO
+
+insert CONSULTA values (GETDATE(),3,8,1,null,1)
